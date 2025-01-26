4093ee68fc5f75d6d471a44e294a463e383e6c9d
arpachuilo <andrew.pachuilo@gmail.com>
2025-01-26 11:06:44 +0100

    Added telegraph

--- enemies/bubble/Boss.cs
+++ enemies/bubble/Boss.cs
@@ -30,15 +30,27 @@ public partial class Boss : Node2D {
   private float laserDuration = 0.5f;
 
   [Export]
+  private float telegraphDuration = 1.5f;
+
+  [Export]
   private float laserFireRotationDelay = 0.5f;
+
+  [Export]
+  private float laserTelegraphScale = 0.25f;
+
+  [Export]
+  private float laserActiveScale = 2f;
 
   [Export]
   private int health = 100;
 
   private Player player;
+
   private float attackTimer;
-  private float laserTime = 0f;
+  private float laserTimer = 0f;
   private bool laserActive = false;
+  private float telegraphTimer = 0f;
+  private bool telegraphActive = false;
 
   public override void _Ready() {
     spawner.BossMode = true;
@@ -86,16 +98,34 @@ public partial class Boss : Node2D {
     if (attackTimer <= 0f) {
       SetAttackTimer();
 
+      telegraphActive = true;
+      telegraphTimer = telegraphDuration;
+      laserSprite.Visible = true;
+
+      // telegraph firing
+    } else if (telegraphActive && telegraphTimer > 0f) {
+      telegraphTimer -= (float)delta;
+
+      var a = Mathf.Sin(telegraphTimer) - 0.50f;
+      laserSprite.Modulate = new Color(1f, 1f, 1f, a);
+      laserSprite.Scale = new Vector2(laserTelegraphScale, laserSprite.Scale.Y);
+
+      // telegraph finished
+    } else if (telegraphActive && telegraphTimer <= 0f) {
       // Fire at player
       laserActive = true;
-      laserSprite.Visible = true;
       laserCollider.ProcessMode = ProcessModeEnum.Inherit;
-      laserTime = laserDuration;
-    } else if (laserActive && laserTime > 0f) {
-      var a = Mathf.Sin(laserTime) + 0.25f;
-      laserSprite.Modulate = new Color(1f, 1f, 1f, a);
-      laserTime -= (float)delta;
-    } else if (laserActive && laserTime <= 0f) {
+      laserTimer = laserDuration;
+      telegraphActive = false;
+
+      // laser firing
+    } else if (laserActive && laserTimer > 0f) {
+      laserSprite.Modulate = new Color(1f, 1f, 1f, 1f);
+      laserSprite.Scale = new Vector2(laserActiveScale, laserSprite.Scale.Y);
+      laserTimer -= (float)delta;
+
+      // laser finished
+    } else if (laserActive && laserTimer <= 0f) {
       laserActive = false;
       laserSprite.Visible = false;
       laserCollider.ProcessMode = ProcessModeEnum.Disabled;
