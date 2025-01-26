9f043edffb04268c0b5bea890a43cce381660b3f
arpachuilo <andrew.pachuilo@gmail.com>
2025-01-26 11:58:16 +0100

    auto format

--- player/Player.cs
+++ player/Player.cs
@@ -1,40 +1,67 @@
-using Godot;
 using System;
 using System.Collections.Generic;
+using Godot;
 
-public partial class Player : CharacterBody2D
-{
+public partial class Player : CharacterBody2D {
   private bool useMouse;
-	private float charge = 0;
-	private bool charging = false;
-	private bool allowCharge = true;
+  private float charge = 0;
+  private bool charging = false;
+  private bool allowCharge = true;
   private float chargeCooldownTimer = 0.0f;
-	private bool chargingAnnimation = false;
+  private bool chargingAnnimation = false;
   private bool moving = false;
-	private Vector2 MouseDirection;
-	private Vector2 chargeDirection;
+  private Vector2 MouseDirection;
+  private Vector2 chargeDirection;
   private Vector2 playerRotation;
   private int maxHealth = 100;
   private int health = 100;
   private float randomTimer = 0;
   private Random random = new Random();
-  [Export] private float chargeCooldown = 3f;
-	[Export] private int _speed = 300;
-	[Export] private ChargeDisplay ChargeDisplay;
-  [Export] private float ChargeScale;
-  [Export] private float chargeSpeed;
-  [Export] private float ChargeKillThreshold = 10f;
-  [Export] private float Friction;
-  [Export] private Area2D point;
-  [Export] private bool isInteractive = true;
-  [Export] private bool winningMode = false;
-  [Export] private int damage = 10;
-  [Export] private AudioListener2D playerAudioListener;
 
-  [Export] public AudioStream chargeSFX;
-  [Export] public AudioStream jumpSFX;
+  [Export]
+  private float chargeCooldown = 3f;
 
-  [Export] private string endScreenPath;
+  [Export]
+  private int _speed = 300;
+
+  [Export]
+  private ChargeDisplay ChargeDisplay;
+
+  [Export]
+  private float ChargeScale;
+
+  [Export]
+  private float chargeSpeed;
+
+  [Export]
+  private float ChargeKillThreshold = 10f;
+
+  [Export]
+  private float Friction;
+
+  [Export]
+  private Area2D point;
+
+  [Export]
+  private bool isInteractive = true;
+
+  [Export]
+  private bool winningMode = false;
+
+  [Export]
+  private int damage = 10;
+
+  [Export]
+  private AudioListener2D playerAudioListener;
+
+  [Export]
+  public AudioStream chargeSFX;
+
+  [Export]
+  public AudioStream jumpSFX;
+
+  [Export]
+  private string endScreenPath;
   private ShaderMaterial playerShader;
 
   private HashSet<IKillable> enemies = new HashSet<IKillable>();
@@ -42,7 +69,7 @@ public partial class Player : CharacterBody2D
   public override void _Ready() {
     point.BodyEntered += OnPointEnter;
     point.BodyExited += OnPointExit;
-		playerShader = playerBodyAnimation.Material as ShaderMaterial;
+    playerShader = playerBodyAnimation.Material as ShaderMaterial;
 
     if (winningMode) {
       WinningAnimation();
@@ -51,36 +78,43 @@ public partial class Player : CharacterBody2D
 
   public void OnPointEnter(Node other) {
     var enemy = other as IKillable;
-    if (enemy == null) return;
+    if (enemy == null)
+      return;
 
     enemies.Add(enemy);
   }
 
   public void OnPointExit(Node other) {
     var enemy = other as IKillable;
-    if (enemy == null) return;
+    if (enemy == null)
+      return;
 
     enemies.Remove(enemy);
   }
 
-	[Export] private AnimationPlayer playerAnimation;
-	[Export] private AnimatedSprite2D playerBodyAnimation;
-	[Export] private AnimatedSprite2D playerFaceAnimation;
-	[Export] private AnimatedSprite2D playerDamageAnimation;
+  [Export]
+  private AnimationPlayer playerAnimation;
 
-	public void Heal(int value)
-	{
-		health += value;
-		healthAnimation();
-	}
-  public void Damage(int damage)
-  {
+  [Export]
+  private AnimatedSprite2D playerBodyAnimation;
+
+  [Export]
+  private AnimatedSprite2D playerFaceAnimation;
+
+  [Export]
+  private AnimatedSprite2D playerDamageAnimation;
+
+  public void Heal(int value) {
+    health += value;
+    healthAnimation();
+  }
+
+  public void Damage(int damage) {
     health -= damage;
-		DamageAnimation();
+    DamageAnimation();
 
-		healthAnimation();
-    if (health <= 90)
-    {
+    healthAnimation();
+    if (health <= 90) {
       Death();
     }
   }
@@ -91,77 +125,72 @@ public partial class Player : CharacterBody2D
     playerFaceAnimation.Play("winning");
   }
 
-	private void healthAnimation() {
-		switch (health)
-		{
-			case 100:
-				playerDamageAnimation.Visible = false;
-				break;
-			case 80:
-				playerDamageAnimation.Visible = true;
-				playerDamageAnimation.Frame = 0;
-				break;
-			case 60:
-				playerDamageAnimation.Visible = true;
-				playerDamageAnimation.Frame = 1;
-				break;
-			case 40:
-				playerDamageAnimation.Visible = true;
-				playerDamageAnimation.Frame = 2;
-				break;
-			case 20:
-				playerDamageAnimation.Visible = true;
-				playerDamageAnimation.Frame = 4;
-				break;
-			default:
-				break;
+  private void healthAnimation() {
+    switch (health) {
+      case 100:
+        playerDamageAnimation.Visible = false;
+        break;
+      case 80:
+        playerDamageAnimation.Visible = true;
+        playerDamageAnimation.Frame = 0;
+        break;
+      case 60:
+        playerDamageAnimation.Visible = true;
+        playerDamageAnimation.Frame = 1;
+        break;
+      case 40:
+        playerDamageAnimation.Visible = true;
+        playerDamageAnimation.Frame = 2;
+        break;
+      case 20:
+        playerDamageAnimation.Visible = true;
+        playerDamageAnimation.Frame = 4;
+        break;
+      default:
+        break;
     }
-	}
+  }
 
-  public void Death()
-  {
+  public void Death() {
     //Play death animation
     //Disable player input
     //Play death sound
     //get the end screen scene
-        Callable
+    Callable
     .From(() => {
       GetTree().ChangeSceneToFile(endScreenPath);
     })
     .CallDeferred();
   }
 
-  public override void _Input(InputEvent @event)
-  {
+  public override void _Input(InputEvent @event) {
     UseMouseCheck(@event);
   }
 
   private void UseMouseCheck(InputEvent @event) {
-    if (@event is InputEventMouseMotion mouseEvent){
+    if (@event is InputEventMouseMotion mouseEvent) {
       useMouse = true;
-    } else if (@event is InputEventJoypadMotion joypadEvent){
+    } else if (@event is InputEventJoypadMotion joypadEvent) {
       useMouse = false;
     }
   }
 
-	public void GetInput()
-	{
+  public void GetInput() {
     MouseDirection = GlobalPosition - GetGlobalMousePosition();
-	}
+  }
 
-	public override void _PhysicsProcess(double delta)
-	{
+  public override void _PhysicsProcess(double delta) {
     CooldownTimerTick(delta);
 
-		if (allowCharge && chargeCooldownTimer <= 0 && charging) {
+    if (allowCharge && chargeCooldownTimer <= 0 && charging) {
       if (charge < 100) {
         charge += chargeSpeed;
       }
 
-			ChargeDisplay.updateCharge(charge);
-		} else if (charge > 0) {
-			charge -= Friction;
-			Velocity = MoveSpeed(charge);
+      ChargeDisplay.updateCharge(charge);
+    } else if (charge > 0) {
+      charge -= Friction;
+      Velocity = MoveSpeed(charge);
     } else if (charge == 0) {
       moving = false;
     }
@@ -172,62 +201,63 @@ public partial class Player : CharacterBody2D
     if (allowCharge == false) {
       playerBodyAnimation.Play("fly");
       playerFaceAnimation.Play("fly");
-			if (charge < 20){
-				 allowCharge = true;
-				playerBodyAnimation.Play("idle");
-				playerFaceAnimation.Play("idle");
-			}
+      if (charge < 20) {
+        allowCharge = true;
+        playerBodyAnimation.Play("idle");
+        playerFaceAnimation.Play("idle");
+      }
     }
 
-		MoveAndSlide();
+    MoveAndSlide();
     DetectCollisions();
-		UpdateShader();
+    UpdateShader();
 
     foreach (var enemy in enemies) {
       var node = enemy as Node;
       if (node != null && !IsInstanceValid(node)) {
-          // cleanup now invalid enemies (can happen if they suicide before you kill)
-          enemies.Remove(enemy);
+        // cleanup now invalid enemies (can happen if they suicide before you kill)
+        enemies.Remove(enemy);
       } else if (!charging && charge > ChargeKillThreshold) {
-          // kill enemies
-          enemy.Hit(damage);
-          enemies.Remove(enemy);
+        // kill enemies
+        enemy.Hit(damage);
+        enemies.Remove(enemy);
       }
     }
-	}
+  }
 
   private void DetectCollisions() {
-    if (!moving){
+    if (!moving) {
       return;
     }
-    for (int i = 0; i < GetSlideCollisionCount(); i++)
-      {
-        var collisionLayer = -1;
-        var collision = GetSlideCollision(i);
+    for (int i = 0; i < GetSlideCollisionCount(); i++) {
+      var collisionLayer = -1;
+      var collision = GetSlideCollision(i);
 
-        // basic static body
-        var collider = collision.GetCollider() as CollisionObject2D;
-        if (collider != null) {
-          collisionLayer = (int) collider.CollisionLayer;
-        } else {
-          var tileMapLayer = collision.GetCollider() as TileMapLayer;
-          if (tileMapLayer == null) return;
+      // basic static body
+      var collider = collision.GetCollider() as CollisionObject2D;
+      if (collider != null) {
+        collisionLayer = (int)collider.CollisionLayer;
+      } else {
+        var tileMapLayer = collision.GetCollider() as TileMapLayer;
+        if (tileMapLayer == null)
+          return;
 
-          var tileSet = tileMapLayer.TileSet;
-          collisionLayer = (int) tileSet.GetPhysicsLayerCollisionLayer(0);
-          /* coTileMapLayer tileMapLayer.CollisionLayer; */
-        }
-
-        if (collisionLayer == -1) return;
-
-        if (collisionLayer == 8){
-          BentAnimation();
-          Velocity = Vector2.Zero;
-          charge = 0;
-          allowCharge = true;
-          moving = false;
-        }
+        var tileSet = tileMapLayer.TileSet;
+        collisionLayer = (int)tileSet.GetPhysicsLayerCollisionLayer(0);
+        /* coTileMapLayer tileMapLayer.CollisionLayer; */
       }
+
+      if (collisionLayer == -1)
+        return;
+
+      if (collisionLayer == 8) {
+        BentAnimation();
+        Velocity = Vector2.Zero;
+        charge = 0;
+        allowCharge = true;
+        moving = false;
+      }
+    }
   }
 
   private void SetMoveCooldown() {
@@ -240,14 +270,12 @@ public partial class Player : CharacterBody2D
     }
   }
 
-	private Vector2 MoveSpeed(float charge)
-	{
-		float chargeScale = charge * ChargeScale;
-		return chargeScale  * chargeDirection;
-	}
+  private Vector2 MoveSpeed(float charge) {
+    float chargeScale = charge * ChargeScale;
+    return chargeScale * chargeDirection;
+  }
 
-  public override void _Process(double delta)
-  {
+  public override void _Process(double delta) {
     if (!isInteractive) {
       return;
     }
@@ -261,30 +289,26 @@ public partial class Player : CharacterBody2D
         playerRotation.X = Input.GetJoyAxis(0, JoyAxis.LeftX);
         playerRotation.Y = Input.GetJoyAxis(0, JoyAxis.LeftY);
 
-        if (Mathf.Abs(playerRotation.X) >= 0.09 && Mathf.Abs(playerRotation.Y) >= 0.09){
+        if (Mathf.Abs(playerRotation.X) >= 0.09 && Mathf.Abs(playerRotation.Y) >= 0.09) {
           Rotation = Mathf.Atan2(playerRotation.Y, playerRotation.X) + Mathf.Pi / 2;
         }
       }
     }
 
-		if (!charging)
-		{
-			IdleAnimation(delta);
-		}
+    if (!charging) {
+      IdleAnimation(delta);
+    }
 
-    if (Input.IsActionPressed("chargeButton"))
-    {
-			if (allowCharge && !moving) {
+    if (Input.IsActionPressed("chargeButton")) {
+      if (allowCharge && !moving) {
         if (!charging) {
           AudioManager.PlaySFX(chargeSFX, 1, false, GlobalPosition);
         }
-				charging = true;
-				JumpAnimation(true);
-			}
-    }
-    else if (Input.IsActionJustReleased("chargeButton"))
-    {
-		  JumpAnimation(false);
+        charging = true;
+        JumpAnimation(true);
+      }
+    } else if (Input.IsActionJustReleased("chargeButton")) {
+      JumpAnimation(false);
       chargeDirection.X = Mathf.Sin(Rotation);
       chargeDirection.Y = -Mathf.Cos(Rotation);
 
@@ -298,90 +322,78 @@ public partial class Player : CharacterBody2D
     }
   }
 
-	private void JumpAnimation(bool on)
-	{
-		if (on && chargingAnnimation == false)
-		{
-			playerBodyAnimation.Play("jump");
-			playerFaceAnimation.Play("jump");
-			chargingAnnimation = true;
-		}
-		else if (!on && chargingAnnimation == true)
-		{
-			playerAnimation.Stop();
-			playerBodyAnimation.Stop();
-			playerFaceAnimation.Stop();
-			chargingAnnimation = false;
-		}
+  private void JumpAnimation(bool on) {
+    if (on && chargingAnnimation == false) {
+      playerBodyAnimation.Play("jump");
+      playerFaceAnimation.Play("jump");
+      chargingAnnimation = true;
+    } else if (!on && chargingAnnimation == true) {
+      playerAnimation.Stop();
+      playerBodyAnimation.Stop();
+      playerFaceAnimation.Stop();
+      chargingAnnimation = false;
+    }
 
-		if (charge > 80 && chargingAnnimation == true && on)
-		{
-			playerAnimation.Play("jump");
-		}
-	}
+    if (charge > 80 && chargingAnnimation == true && on) {
+      playerAnimation.Play("jump");
+    }
+  }
 
-	private void IdleAnimation(double delta)
-	{
+  private void IdleAnimation(double delta) {
     // Decrease the timer
     randomTimer -= (float)delta;
 
     // When the timer reaches 0, decide whether to play the animation
-    if (randomTimer <= 0)
-    {
-        // Random chance to play the animation
-        if (random.NextDouble() > 0.5)
-        {
-					playerBodyAnimation.Play("idle");
-          playerFaceAnimation.Play("idle");
-        }
+    if (randomTimer <= 0) {
+      // Random chance to play the animation
+      if (random.NextDouble() > 0.5) {
+        playerBodyAnimation.Play("idle");
+        playerFaceAnimation.Play("idle");
+      }
 
-        // Reset the timer with a random interval (e.g., 1 to 5 seconds)
-        randomTimer = (float)random.NextDouble() * 2 + 1;
+      // Reset the timer with a random interval (e.g., 1 to 5 seconds)
+      randomTimer = (float)random.NextDouble() * 2 + 1;
     }
-	}
+  }
 
-	public void BentAnimation()
-	{
-			playerBodyAnimation.Play("bent");
-			playerFaceAnimation.Play("bent");
-			playerAnimation.Play("jump");
+  public void BentAnimation() {
+    playerBodyAnimation.Play("bent");
+    playerFaceAnimation.Play("bent");
+    playerAnimation.Play("jump");
 
-			Timer timer = new Timer();
-			AddChild(timer);
-			timer.WaitTime = 0.3;
-			timer.OneShot = true;
-			timer.Connect("timeout", Callable.From(OnBentAnimationTimeout));
-			timer.Start();
-	}
+    Timer timer = new Timer();
+    AddChild(timer);
+    timer.WaitTime = 0.3;
+    timer.OneShot = true;
+    timer.Connect("timeout", Callable.From(OnBentAnimationTimeout));
+    timer.Start();
+  }
 
-	private void OnBentAnimationTimeout()
-	{
-			playerAnimation.Stop();
-			playerBodyAnimation.Play("idle");
-			playerFaceAnimation.Play("idle");
-	}
+  private void OnBentAnimationTimeout() {
+    playerAnimation.Stop();
+    playerBodyAnimation.Play("idle");
+    playerFaceAnimation.Play("idle");
+  }
 
-	public void DamageAnimation()
-	{
-			playerFaceAnimation.Play("damage");
+  public void DamageAnimation() {
+    playerFaceAnimation.Play("damage");
 
-			Timer timer = new Timer();
-			AddChild(timer);
-			timer.WaitTime = 0.6;
-			timer.OneShot = true;
-			timer.Connect("timeout", Callable.From(OnDamageAnimationTimeout));
-			timer.Start();
-	}
+    Timer timer = new Timer();
+    AddChild(timer);
+    timer.WaitTime = 0.6;
+    timer.OneShot = true;
+    timer.Connect("timeout", Callable.From(OnDamageAnimationTimeout));
+    timer.Start();
+  }
 
-	private void OnDamageAnimationTimeout()
-	{
-			playerFaceAnimation.Play("idle");
-	}
-	private void UpdateShader()
-	{
-    float hp01 = (float) health/ (float) maxHealth;
+  private void OnDamageAnimationTimeout() {
+    playerFaceAnimation.Play("idle");
+  }
+
+  private void UpdateShader() {
+    float hp01 = (float)health / (float)maxHealth;
 
     RenderingServer.GlobalShaderParameterSet("swirl_strength", Mathf.Max(1.0f - hp01, 0.01f));
-		playerShader.SetShaderParameter("health", hp01);
-	}
+    playerShader.SetShaderParameter("health", hp01);
+  }
 }
