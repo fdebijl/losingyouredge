a242a12689aa452bad07294ae34cc024c09f0d5a
Floris de Bijl <floris.debijl@gmail.com>
2025-01-26 11:07:49 +0100

    Add TriggerEveryTime bool to SpawnerTriggers

--- levels/tools/SpawnerActivatorTrigger.cs
+++ levels/tools/SpawnerActivatorTrigger.cs
@@ -2,13 +2,17 @@ using Godot;
 
 public partial class SpawnerActivatorTrigger : Area2D {
   [Export] public Godot.Collections.Array<BubbleSpawner> spawners;
+  [Export] public bool TriggerEveryTIme = false;
+
+  private bool _hasTriggered = false;
 
   public void On_SpawnerActivatorTrigger_body_entered(Node body) {
-    GD.Print("SpawnerActivatorTrigger body entered");
     if (body is Player) {
-      GD.Print("SpawnerActivatorTrigger body is player");
       foreach (BubbleSpawner spawner in spawners) {
-        spawner.IsActive = true;
+        if (TriggerEveryTIme || !_hasTriggered) {
+          spawner.IsActive = true;
+          _hasTriggered = true;
+        }
       }
     }
   }
